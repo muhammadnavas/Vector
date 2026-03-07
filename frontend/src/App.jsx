4e0ff28a11c9971bcdfbe1fcf5731f0f50864149
MrHidey <navasns0409@gmail.com>
2026-03-07 15:50:22 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,38 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-function App() {
-  return (
-    <>
-      {/* Full-viewport background canvas */}
-      <div style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        zIndex: 0,
-        background: '#0a0a0f',   /* dark base so waves pop */
-      }}>
-        <FloatingLines
-          enabledWaves={['top', 'middle', 'bottom']}
-          lineCount={5}
-          lineDistance={5}
-          bendRadius={5}
-          bendStrength={-0.5}
-          interactive={true}
-          parallax={true}
-        />
-      </div>
-
-      {/* Page content sits above the background */}
-      <div style={{ position: 'relative', zIndex: 1 }}>
-        {/* your page content goes here */}
-      </div>
-    </>
-  );
-}
-
-export default App;
